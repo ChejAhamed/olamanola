5f921618958979b344085a86d2517aa8960f2dc5
Chej Ahamed <chejahamed@gmail.com>
2021-11-23 16:21:10 +0000

    Add style to header scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './Header.scss'
 const Header = function () {
   return (
-    <div className="header__nav-container">
-      <h1>HEADEER</h1>
-      <nav className="header__navegator">
-        <Link className="header__nav-link" to="/">Dashboard</Link>
-        <Link className="header__nav-link" to="/contactus">Contact Us</Link>
-        <Link className="header__nav-link" to="/services">Services</Link>
-        <Link className="header__nav-link" to="/gallery">Gallery</Link>
-      </nav>
-    </div>
+    <header className="header">
+        <div className="header__nav-container">
+          <h1>HEADEER</h1>
+          <nav className="header__navegator">
+            <Link className="header__nav-link" to="/">Dashboard</Link>
+            <Link className="header__nav-link" to="/contactus">Contact Us</Link>
+            <Link className="header__nav-link" to="/services">Services</Link>
+            <Link className="header__nav-link" to="/gallery">Gallery</Link>
+          </nav>
+        </div>
+    </header>
   );
 };
 
