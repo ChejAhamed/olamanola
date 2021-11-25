785b4950e3309935110d8718d40e7c9ee9ab4679
Chej Ahamed <chejahamed@gmail.com>
2021-11-25 11:20:33 +0000

    Add styled-compoent nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,95 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.scss'
-const Header = function () {
+import React, { useState } from "react";
+import styled from "styled-components";
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="header">
-       
-          
-          
-          <div className="header__logo"> 
-          
-          </div>
-          
-          <nav className="header__navegator">
-            <Link className="header__nav-link" to="/">Dashboard</Link>
-            <Link className="header__nav-link" to="/contactus">Contact Us</Link>
-            <Link className="header__nav-link" to="/services">Services</Link>
-            <Link className="header__nav-link" to="/gallery">Gallery</Link>
-          </nav>
-        
-    </header>
+    <Nav>
+      <Logo >
+        <img src="../images/ola.svg" alt="logo" />
+      </Logo>
+      <Hamburger onClick={() => setIsOpen(!isOpen)}>
+        <span />
+        <span />
+        <span />
+      </Hamburger>
+      <Menu isOpen={isOpen}>
+        <MenuLink href="/">Home</MenuLink>
+        <MenuLink href="/gallery">Gallery</MenuLink>
+        <MenuLink href="/services">Services</MenuLink>
+        <MenuLink href="/contactus">Contact Us</MenuLink>
+      </Menu>
+    </Nav>
   );
-};
+}
 
 export default Header;
+
+const MenuLink = styled.a`
+  padding: 1rem 2rem;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  color: #67bc98;
+  transition: all 0.3s ease-in;
+  font-size: 0.9rem;
+  &:hover {
+    color: #7b7fda;
+  }
+`;
+
+const Nav = styled.div`
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  background: white;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Logo = styled.a`
+  padding: 1rem 0;
+  color: #7b7fda;
+  text-decoration: none;
+  font-weight: 800;
+  font-size: 1.7rem;
+  background-img: url('../images/olaa.svg')
+  span {
+    font-weight: 300;
+    font-size: 1.3rem;
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  @media (max-width: 768px) {
+    overflow: hidden;
+    flex-direction: column;
+    max-height: ${({ isOpen }) => (isOpen ? "300px" : "0")};
+    transition: max-height 0.3s ease-in;
+    width: 100%;
+  }
+`;
+
+const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  span {
+    height: 2px;
+    width: 25px;
+    background: #7b7fda;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
