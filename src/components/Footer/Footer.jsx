e597c24954d19d7d4ac631af666b794300a2d84a
Chej Ahamed <chejahamed@gmail.com>
2021-11-26 12:39:59 +0000

    Update color  MenuLInk in header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 import './Footer.scss'
 const Footer = function () {
   return (
-    <div className="footer">
+    <footer className="footer">
       <h2>FOOOOOOOOOOOOOOOOOOOOTER</h2>
-    </div>
+      <div className="footer__social-media">
+        <SocialIcon url="https://facebook.com/olamanola"  />
+        <SocialIcon url="https://instagram.com/olamanola" />
+        <SocialIcon url="https://twitter.com/olamanola" />
+        <SocialIcon url="https://youtube.com/olamanola" />
+      </div>
+      
+    </footer>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -30,21 +30,21 @@ const MenuLink = styled.a`
   cursor: pointer;
   text-align: center;
   text-decoration: none;
-  color: #67bc98;
+  color: black;
   transition: all 0.3s ease-in;
   font-size: 0.9rem;
   &:hover {
-    color: #7b7fda;
+    color: brown;
   }
 `;
 
 const Nav = styled.div`
   padding: 0 2rem;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-between;;
   align-items: center;
   flex-wrap: wrap;
-  background: white;
+  background: #FEEEEA;
   position: absolute;
   top: 0;
   left: 0;
